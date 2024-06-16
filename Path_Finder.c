8c1b1484e466ea782d265c696924b74c2579eb67
Amritesh Bhardwaj <bhamritesh@gmail.com>
2024-06-17 00:13:13 +0530

    added comments

--- Path_Finder.c
+++ Path_Finder.c
@@ -1,62 +1,63 @@
 #include <stdio.h>
-
-void findShortestPath(int cr, int cc, int er, int ec, char path[], int pathLen, char shortestPath[], int *shortestPathLen) {
-    if (cr == er && cc == ec) { // base case: reached the end point
-        path[pathLen] = '\0'; // Null-terminate the path string
-        if (pathLen < *shortestPathLen) {
+#include <string.h>
+// cc=current column | cr=current row | rr = required row | rc = required column
+void findShortestPath(int cr, int cc, int rr, int rc, char path[], int pathLen, char shortestPath[], int *shortestPathLen)
+{
+    if (cr == rr && cc == rc)
+    { // base case: reached the end point
+        path[pathLen] = '\0';
+        // if the latest path found is shorter than the current stored one
+        if (pathLen < *shortestPathLen)
+        {
             *shortestPathLen = pathLen;
-            for (int i = 0; i <= pathLen; i++) {
-                shortestPath[i] = path[i];
-            }
+            strcpy(shortestPath, path);
         }
         return;
     }
-    
-    if (cr < er) { // move down if not at the last row
+
+    if (cr < rr)
+    { // move down if not at the last row
         path[pathLen] = 'D';
-        findShortestPath(cr + 1, cc, er, ec, path, pathLen + 1, shortestPath, shortestPathLen);
+        findShortestPath(cr + 1, cc, rr, rc, path, pathLen + 1, shortestPath, shortestPathLen);
     }
-    
-    if (cc < ec) { // move right if not at the last column
+
+    if (cc < rc)
+    { // move right if not at the last column
         path[pathLen] = 'R';
-        findShortestPath(cr, cc + 1, er, ec, path, pathLen + 1, shortestPath, shortestPathLen);
+        findShortestPath(cr, cc + 1, rr, rc, path, pathLen + 1, shortestPath, shortestPathLen);
     }
 }
 
-int main() {
+int main()
+{
     int n; // number of rows
     printf("Enter number of rows of the maze: ");
     scanf("%d", &n);
-    
+
     int m; // number of columns
     printf("Enter number of columns of the maze: ");
     scanf("%d", &m);
 
-    int startX, startY, endX, endY;
+    int startX, startY, desX, desY;
+    printf("Enter the starting coordinates (x, y): ");
+    scanf("(%d, %d)", &startX, &startY);
+    printf("Enter the ending coordinates (x, y): ");
+    scanf("(%d, %d)", &desX, &desY);
 
-    // Input starting coordinates
-    printf("Enter the starting row: ");
-    scanf("%d", &startX);
-    printf("Enter the starting column: ");
-    scanf("%d", &startY);
-
-    // Input ending coordinates
-    printf("Enter the ending row: ");
-    scanf("%d", &endX);
-    printf("Enter the ending column: ");
-    scanf("%d", &endY);
-
-    char path[n + m]; // buffer to store the current path
-    char shortestPath[n + m]; // buffer to store the shortest path
+    char path[n + m];           // stores the current path
+    char shortestPath[n + m];   // stores the shortest path
     int shortestPathLen = 1000; // initialize the shortest path length to a large value
 
-    findShortestPath(startX, startY, endX, endY, path, 0, shortestPath, &shortestPathLen);
+    findShortestPath(startX, startY, desX, desY, path, 0, shortestPath, &shortestPathLen);
 
-    if (shortestPathLen == 1000) {
-        printf("No path found!\n");
-    } else {
+    if (shortestPathLen == 1000)
+    {
+        printf("No path could be found :[ !\n");
+    }
+    else
+    {
         printf("Shortest path: %s\n", shortestPath);
     }
-    
+
     return 0;
 }
